f8611020ad58400738834b8b1984fca033fb3869
ADU <LukeDu571@gmail.com>
2018-04-01 21:51:54 +0800

    add new user to mysql, parameter should change to request body

--- src/main/java/com/adu21/ddd/controller/UserController.java
+++ src/main/java/com/adu21/ddd/controller/UserController.java
@@ -1,19 +1,25 @@
 package com.adu21.ddd.controller;
 
 import com.adu21.ddd.domain.User;
+import com.adu21.ddd.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/${api.version}/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<User> register(@RequestBody User user){
+    public ResponseEntity<User> register(@RequestParam String name, @RequestParam String pwd){
+        User user = new User();
+        user.setUserName(name);
+        user.setPassWord(pwd);
+        userRepository.save(user);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
--- src/main/java/com/adu21/ddd/domain/User.java
+++ src/main/java/com/adu21/ddd/domain/User.java
@@ -2,11 +2,20 @@ package com.adu21.ddd.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
     private String userName;
     private String passWord;
 }
